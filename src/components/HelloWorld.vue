1f53fc0f501bf544c3d4f23e1e5ff4aad2b9fb26
Donald Chow <donald7290@gmail.com>
2023-08-20 21:58:04 +0900

    Setup bootstrap and font-awesome

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div class="btn btn-primary">Hello World</div>
+    <div><i class="fa-solid fa-house"></i></div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
