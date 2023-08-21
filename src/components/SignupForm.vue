e734fb7809697ab7a20deb71bf946f23bc2d9c45
Donald Chow <donald7290@gmail.com>
2023-08-21 23:33:36 +0900

    Created Sign up Page

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="border p-4 bg-light col-6">
+    <form class="h-100 d-flex flex-column justify-content-center">
+      <h1 class="mb-3">Sign up now!</h1>
+      <div class="text-center mb-3">No credit card required!.</div>
+      <div class="mb-3">
+        <input type="email" class="form-control" v-model="email" placeholder="Email">
+      </div>
+      <div class="mb-3">
+        <input type="password" class="form-control" v-model="password" placeholder="Password">
+      </div>
+      <div class="d-flex justify-content-end mb-3">
+
+        <p>By creating an account, you agree to the Dinelemma Privacy Policy and Terms of Service.</p>
+      </div>
+      <div class="d-grid">
+        <button type="submit" class="btn btn-primary btn-lg">Start now</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SigninForm',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+a {
+  color: $primary;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SigninView from '../views/SigninView.vue'
+import SignupView from '../views/SignupView.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/signin',
     name: 'SigninView',
     component: SigninView
+  },
+  {
+    path: '/signup',
+    name: 'SignupView',
+    component: SignupView
   },
   {
     path: '/about',
