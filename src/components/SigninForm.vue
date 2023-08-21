93bf62d4029669c7179774fbbfc35ebd2e4f3f22
Donald Chow <donald7290@gmail.com>
2023-08-21 23:33:27 +0900

    minor changes to sign in page

--- src/components/SigninForm.vue
+++ src/components/SigninForm.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="border p-4 bg-light col-4 m-auto">
-    <form>
+  <div class="border p-4 bg-light col-6">
+    <form class="h-100 d-flex flex-column justify-content-center">
+      <h1 class="mb-3">Login</h1>
+      <div class="text-center mb-3">New to Dinelemma?<router-link to="/signup">Sign up today</router-link>.</div>
       <div class="mb-3">
         <input type="email" class="form-control" v-model="email" placeholder="Email">
       </div>
       <div class="mb-3">
         <input type="password" class="form-control" v-model="password" placeholder="Password">
       </div>
-      <div class="d-flex justify-content-between mb-3">
-        <router-link to="/signup">Signup</router-link>
+      <div class="d-flex justify-content-end mb-3">
+
         <router-link to="/forget">Forgot Password?</router-link>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="d-grid">
+        <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+      </div>
     </form>
   </div>
 </template>
--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="signin">
-    <h1>This is an Signin Page</h1>
-    <SigninForm />
+
+    <div class="row">
+      <div class="col-6 d-flex flex-column justify-content-around">
+        <img
+          src="https://cdn.discordapp.com/attachments/1112292984747204731/1143184403984568352/daforcez_logo_for_an_app_use_2_colors_only_use_ED7277_and_EAD2C_bea522cc-368c-4148-9da9-70dc115440f9.png"
+          alt="" class="w-100" style="mix-blend-mode: screen;">
+        <h4>Can decide where to eat? Let Dinelemma solve your Dilemma.</h4>
+      </div>
+      <SigninForm />
+    </div>
   </div>
 </template>
 
@@ -16,3 +24,6 @@ export default {
   }
 }
 </script>
+
+<style>
+</style>
