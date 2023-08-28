6a2ee70c779eadb9dcf8ebe7a47d57ac908c1f93
Donald Chow <donald7290@gmail.com>
2023-08-28 23:40:25 +0900

    Added Dashboard page if user logged in

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+    <h1>This is the dashboard</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'DashboardView',
+  computed: {
+    ...mapGetters(['user'])
+  }
+}
+</script>
