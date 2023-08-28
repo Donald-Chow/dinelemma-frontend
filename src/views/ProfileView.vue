2e564d43d8db95daf41347d0c77c5dcbf3d619c7
Donald Chow <donald7290@gmail.com>
2023-08-28 23:38:41 +0900

    Added a profile page

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Profile!</h1>
+  <h2>Name: {{ user.name }}</h2>
+  <h2>Username: {{ user.username }}</h2>
+  <h2>Email: {{ user.email }}</h2>
+  <button @click="handleLogout" class="btn btn-primary btn-lg" v-if="user">Log out! <i
+      class="fa-solid fa-arrow-right-from-bracket"></i></button>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ProfileView',
+  computed: {
+    ...mapGetters(['user'])
+  },
+  methods: {
+    handleLogout() {
+      localStorage.removeItem('Authorization');
+      this.$store.dispatch('user', null)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
