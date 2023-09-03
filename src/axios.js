8496614a69e13c65a1c53328d3f9b82791b583ac
Donald Chow <donald7290@gmail.com>
2023-09-03 13:29:05 +0900

    Session expire will ask use to signin again

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,18 @@
 import axios from 'axios'
+import router from '@/router'
 
 axios.defaults.baseURL = 'http://localhost:3000/';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('Authorization');
+axios.interceptors.response.use(
+  function (response) {
+    return response; // Don't forget to return the response
+  },
+  function (error) {
+    // if the signature expire, ask the use to sign in again
+    if (error.response.data == 'Signature has expired') {
+      window.alert('Your session has expired')
+      router.push({ name: 'SigninView'});
+    }
+    return Promise.reject(error); // Reject the promise to propagate the error
+  }
+);
