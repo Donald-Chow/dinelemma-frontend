099c0f85a78e96f74615023c26d48dd972cd1ab7
Donald Chow <donald7290@gmail.com>
2023-09-08 18:51:30 +0900

    Turn RestaurantList into ExploreRestaurant instead

---
+++ src/components/RestaurantList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Restaurant Lists
+    <div v-for="restaurant in restaurant_list" :key="'restaurant_' + restaurant.name"
+      class="d-flex justify-content-around align-items-center bg-white">
+      <div>
+        <h2>{{ restaurant.name }}</h2>
+        <p>{{ restaurant.address }}</p>
+      </div>
+      <div class="d-flex flex-column ">
+        <div class="btn btn-primary">+</div>
+        <div class="btn btn-primary">-</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'RestaurantList',
+  components: {
+
+  },
+  data() {
+    return {
+      restaurant_list: [{ name: "foo", address: "bar" }, { name: "Hello", address: "world" }],
+
+    }
+  }
+}
+</script>
---
+++ src/views/ExploreRestaurant.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>SEARCH</h1>
+  <form @submit.prevent="handleSearch" class="mb-3">
+    <input type="text" v-model="query" class="rounded">
+    <button type="submit" class="rounded">Search</button>
+  </form>
+  <RestaurantCard v-for="restaurant in  results " :key="restaurant.place_id" :restaurant="restaurant" />
+</template>
+
+<script >
+import googleMapsApi from '@/axios.js';
+import RestaurantCard from '@/components/RestaurantCard.vue';
+
+export default {
+  name: 'ExploreRestaurant',
+  data() {
+    return {
+      query: '',
+      results: []
+    }
+  },
+  components: {
+    RestaurantCard
+  },
+  methods: {
+    async handleSearch() {
+      console.log("searching");
+      const response = await googleMapsApi.get('', {
+        params: {
+          query: this.query,
+        },
+      })
+      this.results = response.data.results
+    }
+  }
+}
+</script>
