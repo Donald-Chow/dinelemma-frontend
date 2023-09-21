c9fca487851eb1ddf2f62ac50529ff9f58582490
Donald Chow <donald7290@gmail.com>
2023-09-21 12:16:05 +0900

    create a primary button

---
+++ src/components/Shared/ButtonPrimary.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button class="btn-primary">
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonPrimary',
+  props: ['text']
+}
+</script>
+
+<style scoped lang="scss">
+.btn-primary {
+  background-color: $primary;
+  color: $white;
+  border-radius: 50px;
+  height: 48px;
+  width: 270px;
+  text-align: center;
+  font-family: inherit;
+  font-size: 18px;
+  border: none;
+
+  transition: background-color .15s ease-in-out;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: $primary-darken
+  }
+}
+</style>
