e804374751c9ea137aa8974493e69738275ac973
Donald Chow <donald7290@gmail.com>
2023-10-22 22:53:08 +0900

    Implemented toggle new group form and search function

--- src/views/GroupList.vue
+++ src/views/GroupList.vue
@@ -3,24 +3,23 @@
     <h1>
       My Groups
     </h1>
-    <!-- Button trigger modal -->
-    <div>
-      <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#newGroupForm">
+    <div class="search-bar-wrapper">
+      <form class="search-bar" @submit.prevent="filterGroups">
+        <input class="search-bar" type="text" placeholder="Search" v-model="searchInput" @input="filterGroups">
+      </form>
+      <button type="button" @click.prevent="toggleNewGroup">
         <i class="fa-solid fa-plus"></i>
       </button>
     </div>
   </div>
   <div>
-    <div style="border-bottom: 1px solid pink;background-color: gray;">
-      This Is Search bar
-    </div>
-    <div v-for="group in groups" :key="group.id">
+    <div v-for="group in showGroups" :key="group.id">
       <GroupCard :group="group" />
     </div>
   </div>
 
   <!-- Modal -->
-  <GroupForm :users="users" @notice="emitNotice" />
+  <GroupForm :users="users" @notice="emitNotice" v-if="showForm" />
 </template>
 
 <script>
@@ -33,7 +32,10 @@ export default {
   data() {
     return {
       groups: [],
-      users: []
+      showGroups: [],
+      users: [],
+      showForm: false,
+      searchInput: ""
     }
   },
   components: {
@@ -50,6 +52,7 @@ export default {
         const response = await axios.get('groups')
         // console.log(response);
         this.groups = response.data.groups
+        this.showGroups = this.groups
         this.users = response.data.users
       } catch (error) {
         console.error('An error occurred while fetching groups:', error);
@@ -57,7 +60,41 @@ export default {
     },
     emitNotice(message) {
       this.$emit('notice', message)
+    },
+    toggleNewGroup() {
+      this.showForm = !this.showForm
+    },
+    filterGroups() {
+      if (this.searchInput == "") {
+        this.showGroups = this.groups
+      } else {
+        this.showGroups = this.groups.filter((group) => {
+          return group.name.toLowerCase().includes(this.searchInput.toLowerCase())
+        })
+      }
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.search-bar-wrapper {
+  display: flex;
+  justify-content: space-between;
+
+  .search-bar {
+    flex-grow: 1;
+    margin-right: 16px;
+
+    input {
+      border: none;
+      background-color: $light-gray;
+      box-shadow: 1px solid black;
+      flex-grow: 1;
+      border-radius: 4px;
+      padding: 6px;
+      width: 100%;
+    }
+  }
+}
+</style>
