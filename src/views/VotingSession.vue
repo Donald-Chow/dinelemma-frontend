47240acb6d474d41806e386dff1407f7e2162625
Donald Chow <donald7290@gmail.com>
2024-01-20 21:44:08 +0900

    Fixed styling to voting result page

--- src/views/VotingSession.vue
+++ src/views/VotingSession.vue
@@ -3,7 +3,21 @@
     <h1>Voting Session {{ session.id }}</h1>
   </div>
 
-  <div v-if="!session.restaurant_id">
+  <div v-if="session.restaurant_id">
+    <router-link :to="{ name: 'GroupDetails', params: { id: session.group_id } }">
+      <ButtonPrimary text="BACK" />
+    </router-link>
+    <h3>Chosen Restaurant: </h3>
+    <h3><strong>{{ restaurant.name }}</strong></h3>
+    <img :src="restaurantImageURL" v-if="restaurantImageURL">
+    <img
+      :src="'https://source.unsplash.com/featured/?' + restaurant.category + '&food&' + Math.floor(Math.random() * 1000)"
+      alt="" v-else>
+    <!-- <img :src="restaurant.photos[0].getUrl({ maxWidth: 1080, maxHeight: 1080 })" alt=""> -->
+
+  </div>
+
+  <div v-else>
     <div class="vote-wrapper">
       <VotingCard class="vote-card" v-for="vote in votes" :key="vote.id" :vote="vote" @remove-vote="removeVote" />
     </div>
@@ -13,18 +27,6 @@
     </div>
   </div>
 
-  <div v-if="session.restaurant_id">
-    <router-link :to="{ name: 'GroupDetails', params: { id: session.group_id } }">
-      <div class="btn btn-warning">BACK</div>
-    </router-link>
-    <h3>Chosen Restaurant: <strong>{{ restaurant.name }}</strong></h3>
-    <img :src="restaurantImageURL" class="d-block w-100" v-if="restaurantImageURL">
-    <img
-      :src="'https://source.unsplash.com/featured/?' + restaurant.category + '&food&' + Math.floor(Math.random() * 1000)"
-      alt="" class="w-100" v-else>
-    <!-- <img :src="restaurant.photos[0].getUrl({ maxWidth: 1080, maxHeight: 1080 })" alt=""> -->
-
-  </div>
   <div id="google_vote"></div>
 </template>
 
@@ -32,12 +34,14 @@
 import axios from 'axios';
 import VotingCard from '@/components/VoteSession/VotingCard.vue';
 import { createConsumer } from '@rails/actioncable';
+import ButtonPrimary from '@/components/Shared/ButtonPrimary.vue';
 
 export default {
   name: 'VotingSession',
   props: ['id'],
   components: {
-    VotingCard
+    VotingCard,
+    ButtonPrimary
   },
   emits: ['alert', 'notice'],
   data() {
@@ -92,6 +96,7 @@ export default {
                 // update page
                 this.session = data.vote_session
                 this.restaurant = data.vote_session.restaurant
+                this.getPhotoUrl()
               }
             },
             connected: () => { console.log(`Subscribed to the voteSession with the id ${this.session.id}.`) },
@@ -141,5 +146,12 @@ export default {
 
 .vote-card {
   position: absolute;
+}
+
+img {
+  height: 60vh;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 10px;
 }
 </style>
